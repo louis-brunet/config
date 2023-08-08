7aae1b70f9aef4cc21b6ed3ed7ca95e81d83a4ee
Louis Brunet <louis.brunet1@grenoble-inp.org>
2023-08-09 01:21:09 +0200

    feat(nvim): added shade and luatab plugins

--- nvim/lua/user/plugins/nvim-tree.lua
+++ nvim/lua/user/plugins/nvim-tree.lua
@@ -7,6 +7,10 @@ return {
         config = function()
             vim.g.loaded_netrw = 1
             vim.g.loaded_netrwPlugin = 1
+            vim.cmd.highlight("NvimTreeIndentMarker guifg=#31353f")
+            vim.cmd.highlight("NvimTreeNormal guibg=none")
+            vim.cmd.highlight("NvimTreeEndOfBuffer guibg=none")
+            vim.cmd.highlight("NvimTreeVertSplit guibg=none")
             require('nvim-tree').setup({
                 filters = {
                     git_ignored = false,
@@ -15,6 +19,24 @@ return {
                     sync = {
                         open = true,
                         close = true,
+                    },
+                },
+                renderer = {
+                    group_empty = true,
+                    add_trailing = true,
+                    indent_markers = {
+                        enable = true,
+                    },
+                    icons = {
+                        show = {
+                            folder_arrow = false,
+                        },
+                        glyphs = {
+                            -- folder = {
+                            --     -- arrow_closed = '▶',
+                            --     -- arrow_open = '▼',
+                            -- },
+                        },
                     },
                 },
             })
